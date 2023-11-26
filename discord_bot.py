0241b998d4853aacadae952d2d64a264081ac04a
Taoball <147546613+Taoballl@users.noreply.github.com>
2023-11-27 01:36:29 +0300

    added welcome message along with spam prevention

--- discord_bot.py
+++ discord_bot.py
@@ -12,12 +12,24 @@ async def on_ready():
     print('------')
 
 @bot.event
+async def on_member_join(member):
+    # Send a welcome message when a new member joins
+    welcome_channel = member.guild.system_channel
+    if welcome_channel:
+        welcome_message = f"Welcome to 🗿, {member.mention}! Make sure to read the rules in #welcome 🗿."
+        await welcome_channel.send(welcome_message)
+
+@bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
 
     # React with 🗿 to every message
     await message.add_reaction('🗿')
+
+    # Spam prevention
+    if len(message.content) > 100:  # Adjust the threshold as needed
+        await message.channel.send(f"{message.author.mention}, please avoid spamming!")
 
     await bot.process_commands(message)
 
